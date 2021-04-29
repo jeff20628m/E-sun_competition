d17a672a8df9aa66c796a7c4e89f69bedfb69e9f
Laiyi <44141490+jeff20628m@users.noreply.github.com>
2021-04-29 15:25:00 +0800

    add:new code 2021/04/29

--- Lai/script/load_data.py
+++ Lai/script/load_data.py
@@ -5,36 +5,32 @@ from matplotlib.pyplot import imread
 """Script to preprocess the omniglot dataset and pickle it into an array that's easy
     to index my character type"""
 
-data_path = os.path.join(os.getcwd(), "python")
-train_folder = os.path.join(data_path, 'images_background')
-valpath = os.path.join(data_path, 'images_evaluation')
+data_path = '../../../data_set/模型訓練資料'
+train_folder = os.path.join(data_path, 'format_train')
 
-save_path = os.path.join(os.getcwd(), "save")
+save_path = os.path.join(data_path, 'save')
 
 lang_dict = {}
 
 
 def loadimgs(path, n=0):
-    # if data not already unzipped, unzip it.
-    if not os.path.exists(path):
-        print("unzipping")
-        os.chdir(data_path)
-        os.system("unzip {}".format(path+".zip"))
+    
     X = []
     y = []
     cat_dict = {}
     lang_dict = {}
     curr_y = n
-    # we load every alphabet seperately so we can isolate them later
-    for alphabet in os.listdir(path):
-        print("loading alphabet: " + alphabet)
-        lang_dict[alphabet] = [curr_y, None]
-        alphabet_path = os.path.join(path, alphabet)
+    # we load every word seperately so we can isolate them later
+
+    for word in os.listdir(path): # path is the format training set
+        print("loading word: " + word)
+        lang_dict[word] = [curr_y, None]
+        word_path = os.path.join(path, word)
         # every letter/category has it's own column in the array, so  load seperately
-        for letter in os.listdir(alphabet_path):
-            cat_dict[curr_y] = (alphabet, letter)
+        for letter in os.listdir(word_path):
+            cat_dict[curr_y] = (word, letter)
             category_images = []
-            letter_path = os.path.join(alphabet_path, letter)
+            letter_path = os.path.join(word_path, letter)
             for filename in os.listdir(letter_path):
                 image_path = os.path.join(letter_path, filename)
                 image = imread(image_path)
@@ -47,19 +43,19 @@ def loadimgs(path, n=0):
                 print(e)
                 print("error - category_images:", category_images)
             curr_y += 1
-            lang_dict[alphabet][1] = curr_y - 1
+            lang_dict[word][1] = curr_y - 1
     y = np.vstack(y)
     X = np.stack(X)
     return X, y, lang_dict
 
 
 X, y, c = loadimgs(train_folder)
+print(X,y,c)
+
+#with open(os.path.join(save_path, "train.pickle"), "wb") as f:
+#    pickle.dump((X, c), f)
 
 
-with open(os.path.join(save_path, "train.pickle"), "wb") as f:
-    pickle.dump((X, c), f)
-
-
-X, y, c = loadimgs(valpath)
-with open(os.path.join(save_path, "val.pickle"), "wb") as f:
-    pickle.dump((X, c), f)
+#X, y, c = loadimgs(valpath)
+#with open(os.path.join(save_path, "val.pickle"), "wb") as f:
+#    pickle.dump((X, c), f)
